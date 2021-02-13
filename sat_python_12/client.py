9ea814e1b3a452887b4a109d7da30ca1b45033b2
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-13 20:01:28 +0200

    Add files via upload

---
+++ sat_python_12/client.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+import time, socket, threading
+
+def receving(name, sock, switch):
--- sat_python_12/function_sum.py
+++ sat_python_12/function_sum.py
@@ -1,5 +1,45 @@
-def sum1(a, b):
-	return a + b
-	
-z = sum1(10, 15)
+def sayHello():
+	print('Hello world')
+
+# процедура
+sayHello()
+
+def sum2(a, b):
+	s = a + b
+	return s
+
+z=sum2(1, 2)
 print(z)
+
+z=sum2('a', 'b')
+print(z)
+
+print('a' + 'b')
+
+def exp(a, b):
+	s = a ** b
+	return s
+	
+r=exp(3, 3)
+print(r)
+
+import time
+def test_1(a, b):
+	print('--------#test1----------')
+	et = time.time()
+	res = exp(a, b)
+	st = time.time()
+	dt = st - et
+	print('#test1 time - ', dt)
+	#print('#test1 res - ', res)
+	print('--------#test1----------')
+	
+#test_1(10000000, 10000000)
+
+try:
+	a = 20; b = 11
+	x = a / b
+	print(x)
+except:
+	print('Error. На ноль делить нельзя! ')
+
