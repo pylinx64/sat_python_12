ed4fb7b59c5169e155aab4690c4e71ff35017d31
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-07 11:31:37 +0300

    Add files via upload

--- sat_python_12/program1.py
+++ sat_python_12/program1.py
@@ -1,15 +1,28 @@
-print("Game over")
-print(10)
-print("Hello10")
-print("10")
-
-print(100 + 10)
-print("100" + "10")
-print("Hello" + "user")
-
-x = 100
-text = "Hello, jfoipsfdgjpodfijgoidfsjgoidfshgoidfshgidujfshgiudfshgidfshgihdfigjhdfijghfijgh"
-
+x = 10
+t = 1030
+# первый способ
 print(x)
-print(text)
+print('Деньги', x)
+print('Деньги', x, '$')
+# второй способ
+print('Деньги ' + str(x) + '$')
+# третий способ
+print('Деньги %a$' % x)
+# четвертый способ
+print(f'Деньги {x}$, время {t}, ')
 
+a = [1, 2, 3, 4, 5]
+
+def timeAlg(len_list, speed_algoritm):
+    time = len_list / speed_algoritm 
+    return time
+    
+x=timeAlg(len(a), 3)
+print(f'алгоритм Арефьефа {x} млсек')
+
+def angle(x):
+    a = 90 - x
+    return a, x
+    
+angle1, angle2 = angle(20)
+print(angle1, angle2)
